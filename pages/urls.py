fa7dfda15ebc63acc70b7125bf5deaebff0aca18
GabrielCalcada <gabrielcalcada1@gmail.com>
2021-10-07 13:44:35 -0300

    Fiz algumas mudanças na navbar. Adicionei as páginas: como chegar, fale, praias e sobre. Url e views prontas das pg add.

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from .views import HomePageView
+from .views import sobre
+from .views import praias
+from .views import como_chegar
+from .views import fale
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    # path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('Sobre/', sobre.as_view(), name='sobre'),
+    path('Praias/', praias.as_view(), name='praias'),
+    path('Como-chegar/', como_chegar.as_view(), name='como_chegar'),
+    path('Fale/', fale.as_view(), name='fale'),
+    # path('sobre/', sobre.as_view(), name='sobre')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,16 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name='home.html'
+
+
+class sobre(TemplateView):
+     template_name='sobre.html'
+
+class praias(TemplateView):
+     template_name='praias.html'
+
+class como_chegar(TemplateView):
+     template_name='como_chegar.html'
+
+class fale(TemplateView):
+     template_name='fale.html'
